75b74e28e79e643c1a08068bc7314d1941515052
MatheusAmorim <matheusamorimb2@gmail.com>
2025-03-21 20:46:42 -0400

    backup

--- app/src/main/java/br/com/fiap/ecocalc/dao/FlightEstimateDao.kt
+++ app/src/main/java/br/com/fiap/ecocalc/dao/FlightEstimateDao.kt
@@ -1,15 +1,15 @@
-package br.com.fiap.ecocalc.dao
+//package br.com.fiap.ecocalc.dao
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
-import br.com.fiap.ecocalc.model.FlightEstimate
-
-@Dao
-interface FlightEstimateDao {
-    @Insert
-    fun insert(flightEstimate: FlightEstimate): Long
-
-    @Query("SELECT * FROM flight_estimates")
-    fun getAll(): List<FlightEstimate>
-}
+//import androidx.room.Dao
+//import androidx.room.Insert
+//import androidx.room.Query
+//import br.com.fiap.ecocalc.model.FlightEstimate
+//
+//@Dao
+//interface FlightEstimateDao {
+//    @Insert
+//    fun insert(flightEstimate: FlightEstimate): Long
+//
+//    @Query("SELECT * FROM flight_estimates")
+//    fun getAll(): List<FlightEstimate>
+//}
